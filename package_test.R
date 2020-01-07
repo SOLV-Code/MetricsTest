d9f71175b35cf89896b82cb6051a158d93c9c6e0
SOLV-Code <gottfries.pestal@solv.ca>
2020-01-07 11:25:31 -0800

    documentation added for lt trend simple

--- package_test.R
+++ package_test.R
@@ -11,7 +11,7 @@ pdf("ProbDecl_Fits.pdf",onefile=TRUE,height=8.5, width=11)
 vec.use = c(12,10,14,7,13,5,8,3,4,7,6,5)
 slope.mcmc.fit <- calcPercChangeMCMC(vec.in= vec.use,model.in = NULL ,
                                      perc.change.bm = -25 , na.skip=FALSE,
-                                     out.type = "long", mcmc.plots = FALSE)
+                                     out.type = "long", mcmc.plots = TRUE)
 dev.off()
 
 
