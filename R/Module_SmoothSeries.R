341aba20541eebc7ba6b40ddc541e3112c68b1b6
SOLV-Code <gottfries.pestal@solv.ca>
2020-01-08 00:49:50 -0800

    readme edits

--- R/Module_SmoothSeries.R
+++ R/Module_SmoothSeries.R
@@ -1,17 +1,18 @@
-
-
-
-
+#' smoothSeries
+#'
+#' function to calculate a smoothed version of an input vector (calculate the running geometric mean of log_e
+#' transformed data)
+#' @param vec.in vector with numeric values
+#' @param gen  generation time (i.e. number of years to average)
+#' @param filter.sides time window for gen avg (1 = past values only, 2= centered)
+#' @param out.exp if TRUE, then convert the output back from log space)
+#' @param na.rm if FALSE, then any gen avg including NA values will be NA
+#' @param convergence.check if TRUE, do an automated convergence check
+#' @keywords smoothing,generational average, log transform
+#' @export
 
 smoothSeries <- function(vec.in, gen , filter.sides=2, log.transform = TRUE, out.exp = TRUE,na.rm=FALSE){
-# function to calculate a smoothed version of an input vector
-# smoothing: calculate the geometric mean of log_e transformed
-# NOTE: uses log1p() and expm1() to handle 0 records
-# For discussion see: https://github.com/SOLV-Code/WSP-Metrics-Code/issues/2
-# vec.in= vectors with numbers of fish (not k or M)
-# filter.sides = time window for gen avg (1 = past values only, 2= centered)
-# out.exp = if TRUE, then convert the output back from log space)
-# na.rm = if FALSE, then any gen avg including NA values will be NA
+
 
 if(sum(vec.in < 0,na.rm=TRUE)>0){warning("Function smoothSeries() does not accept negative values");stop()}
 
@@ -25,7 +26,6 @@ vec.out <- as.vector(stats::filter(vec.use, filter=rep(1/gen,gen),sides=filter.s
 
 # default is to convert back from log space (but for slope calcs want to keep in log space)
 if(log.transform & out.exp){vec.out <- expm1(vec.out)}
-
 
 return(vec.out)
 
