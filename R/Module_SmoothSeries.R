c75f3a681cadc48dc92143bb05fd481cbbe831cd
SOLV-Code <gottfries.pestal@solv.ca>
2020-01-08 00:23:06 -0800

    added documentation for JAGS model

--- R/Module_SmoothSeries.R
+++ R/Module_SmoothSeries.R
@@ -1,26 +1,5 @@
-# This function is part of the emerging "WSPMetrics" Package
-# for the latest version and additional information, check
-# https://github.com/SOLV-Code/WSP-Metrics-Code
 
 
-
-# from https://stackoverflow.com/questions/2602583/geometric-mean-is-there-a-built-in
-# not using the function, just here for reference
-gm_mean = function(x, na.rm=TRUE, zero.propagate = FALSE){
-  if(any(x < 0, na.rm = TRUE)){
-    return(NaN)
-  }
-  if(zero.propagate){
-    if(any(x == 0, na.rm = TRUE)){
-      return(0)
-    }
-    exp(mean(log(x), na.rm = na.rm))
-  } else {
-    exp(sum(log(x[x > 0]), na.rm=na.rm) / length(x))
-  }
-}
-
-######################################
 
 
 
